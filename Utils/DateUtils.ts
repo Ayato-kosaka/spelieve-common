459378ed9974ee85c727a56b2da3c9eb0e087830
Ayato-kosaka <53715839+Ayato-kosaka@users.noreply.github.com>
2023-01-02 18:25:15 +0900

    Update DateUtils.ts

--- Utils/DateUtils.ts
+++ Utils/DateUtils.ts
@@ -1,15 +1,13 @@
 export const initialDate = (): Date => new Date(1970, 0, 1, 0, 0, 0);
 export const milliSecondsADay: number = 24 * 60 * 60 * 1000;
-export const formatDateToTime = (date: Date, hourUnit = ':', minUnit = '') => {
-	const [hour, min] = [date.getHours(), date.getMinutes()].map((x) => String(x).padStart(!minUnit ? 2 : 1, '0'));
-	if (date.getHours() == 0 && minUnit !== '') {
-		return min + minUnit;
-	} else {
-		return hour + hourUnit + (parseInt(min) != 0 || !minUnit ? min : '');
-	}
-};
 export const formatToHHMM = (date: Date): string => {
 	return `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
+};
+export const formatToHHMM = (hour: string, min: string): string => {
+	if (hour === '0') {
+		return `${min}Min`;
+	}
+	return `${hour}Hrs${min}`;
 };
 
 export const addition = (base: Date, addition: Date, types: ('Month' | 'Date' | 'Hours' | 'Minutes' | 'Seconds')[]) => {
